e0f04646f5d8cf4b128b74bd564e68f1304d4b1e
Jorge Mosqueda <jorgescript@MacBook-Pro-de-Jorge.local>
2020-08-14 12:28:45 -0500

    Se añadio middleware al tourModel

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,6 @@
 /* IMPORTS */
 const mongoose = require("mongoose");
-const router = require("../routes/tourRoutes");
+const slugify = require("slugify");
 
 /* SCHEMAS */
 const tourSchema = mongoose.Schema(
@@ -10,7 +10,10 @@ const tourSchema = mongoose.Schema(
       unique: [true, "The name must be unique"],
       required: [true, "A tour must have a name"],
       trimp: true,
+      maxlength: [40, "A tour name bust have less or equal then 40 caracters"],
+      minlength: [10, "A tour name bust have more or equal then 10 caracters"],
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, "A tour must have a duration"],
@@ -22,10 +25,16 @@ const tourSchema = mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, "A tour must have a difficulty"],
+      enum: {
+        values: ["easy", "medium", "difficult"],
+        message: "The difficulty must be easy, medium or difficult",
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, "The minimum rating is 1"],
+      max: [5, "The maximum rating is 5"],
     },
     ratingsQuantity: {
       type: Number,
@@ -35,7 +44,15 @@ const tourSchema = mongoose.Schema(
       type: Number,
       required: [true, "A tour must have a price"],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        message: "The discount must be less o equal the price",
+        validator: function (val) {
+          return val <= this.price;
+        },
+      },
+    },
     summary: {
       type: String,
       trimp: true,
@@ -56,7 +73,12 @@ const tourSchema = mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
+  /* propiedades virtuales */
   {
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
@@ -66,6 +88,40 @@ const tourSchema = mongoose.Schema(
 /* VIRTUAL PROPERTIES */
 tourSchema.virtual("durationWeeks").get(function () {
   return this.duration / 7;
+});
+
+/* DOCUMENT MIDDELWARE */
+tourSchema.pre("save", function (next) {
+  /* This apunta la documento actual */
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+/* tourSchema.post("save", function (doc, next) {
+  // doc es el documento actual
+  console.log(doc);
+  next();
+}); */
+
+/* QUERY MIDDLEWARE */
+tourSchema.pre(/^find/, function (next) {
+  /* this es la query actual */
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+/* tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
+  // docs son los documentos actuales
+  console.log(docs);
+  next();
+}); */
+
+/* AGREGATION MIDDLEWARE */
+tourSchema.pre("aggregate", function (next) {
+  /* This apunta al metodo aggregation actual */
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  next();
 });
 
 const Tour = mongoose.model("Tour", tourSchema);
